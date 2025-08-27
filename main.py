75390b8e875f718bc1cf4a7e56a4468a88a5086a
Deepak Prajapat <deepakprajapat7067@gmail.com>
2025-08-27 17:21:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -248,11 +248,9 @@ def page_login():
             st.session_state.logged_in = True
             st.session_state.user = user
             st.success("✅ Login successful! Redirecting to Scraper...")
-            if st.button("➡️ Open Scraper", use_container_width=True):
-                go_to("scraper")
-            # st.session_state.page = "scraper"
-            # time.sleep(0)           # 0 second wait
-            # st.experimental_rerun()   # फिर Scraper page पर rerun
+            st.session_state.page = scraper
+            time.sleep(0)           # 0 second wait
+            st.experimental_rerun()   # फिर Scraper page पर rerun
         else:
             st.error("Invalid credentials")
 
@@ -322,6 +320,7 @@ elif page == "scraper":
     page_scraper()
 else:
     page_home()
+
 
 
 
